90ce3d79080307e59e996578419907808ff542a8
David Kershaw <dk107dk@hotmail.com>
2025-05-26 14:46:37 -0400

    DONE		> are we still challenging the user at startup if no .flightpath file?  Remove. DONE		> added function docs to boolean and counting packages (in CsvPath project) DONE 	> in dark mode the function help is washed out because background too light DONE		> redesigned the help files DONE 	> finished help files 1.0

--- flightpath/util/html_generator.py
+++ flightpath/util/html_generator.py
@@ -1,4 +1,5 @@
 import traceback
+import os
 
 class HtmlGenerator:
 
@@ -7,22 +8,29 @@ class HtmlGenerator:
         page = None
         with open(path, "r", encoding="utf-8") as file:
             page = file.read()
-        page = self.transform(template=page, tokens=tokens)
+        page = self.transform(path=path, template=page, tokens=tokens)
         return page
 
     @classmethod
-    def transform(self, template: str, tokens: dict[str, str] = None) -> str:
+    def transform(self, *, template: str, tokens: dict[str, str] = None, path:str = None) -> str:
         #
         # leave these imports here. they are super slow.
         # so we don't want the latency in testing or ever
         # unless we're actually rendering a template.
         #
-        from jinja2 import Template, TemplateError  # pylint: disable=C0415
-        #import inflect  # pylint: disable=C0415
-
-        #self._engine = inflect.engine()
+        from jinja2 import Template, TemplateError, FileSystemLoader, Environment  # pylint: disable=C0415
+        content = None
         try:
-            t = Template(template)
+            i = path.find(f"{os.sep}help{os.sep}")
+            dirname = path[0:i+6]
+            basename = path[i+6:]
+            print(f"htmlgen: transform: dirname: {dirname}")
+            print(f"htmlgen: transform: basename: {basename}")
+
+            #e = Environment(loader=FileSystemLoader(os.path.dirname(path)))
+            e = Environment(loader=FileSystemLoader(dirname))
+            t = e.get_template( basename )
+            #t = e.get_template(os.path.basename(path))
             content = t.render(data=tokens)
         except TemplateError:
             print(traceback.format_exc())
