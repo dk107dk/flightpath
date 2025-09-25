2ba587ced313b89eb3dd9174cf4224ca482098ed
David Kershaw <dk107dk@hotmail.com>
2025-09-25 18:25:48 -0400

    tweak

--- flightpath/hidden.py
+++ flightpath/hidden.py
@@ -1,3 +1,5 @@
+import traceback
+
 
 #
 # these files need to be referenced so they are found by PyInstaller
@@ -5,28 +7,19 @@
 class Hidden:
 
     def __init__(self, skip=True) -> None:
-        if skip:
-            return
+        #if skip:
+        #    return
         #
         # external backends
         #
         try:
             import paramiko
-        except ImportError:
-            pass
-        try:
             import boto3
             import botocore
-        except ImportError:
-            pass
-        try:
             import azure.storage.blob
-        except ImportError:
-            pass
-        try:
             import google.cloud.storage
         except ImportError:
-            pass
+            print(traceback.format_exc())
         #
         # internal backends
         #
@@ -35,20 +28,24 @@ class Hidden:
             from csvpath.util.s3.s3_data_reader import S3DataReader
             from csvpath.util.s3.s3_data_writer import S3DataWriter
             from csvpath.util.s3.s3_xlsx_data_reader import S3XlsxDataReader
+            from csvpath.util.s3.s3_utils import S3Utils
             from csvpath.util.sftp.sftp_nos import SftpDo
             from csvpath.util.sftp.sftp_data_reader import SftpDataReader
             from csvpath.util.sftp.sftp_data_writer import SftpDataWriter
             from csvpath.util.sftp.sftp_xlsx_data_reader import SftpXlsxDataReader
+            from csvpath.util.sftp.sftp_config import SftpConfig
             from csvpath.util.azure.azure_nos import AzureDo
+            from csvpath.util.azure.azure_utils import AzureUtility
             from csvpath.util.azure.azure_data_reader import AzureDataReader
             from csvpath.util.azure.azure_data_writer import AzureDataWriter
             from csvpath.util.azure.azure_xlsx_data_reader import AzureXlsxDataReader
             from csvpath.util.gcs.gcs_nos import GcsDo
+            from csvpath.util.gcs.gcs_utils import GcsUtility
             from csvpath.util.gcs.gcs_data_reader import GcsDataReader
             from csvpath.util.gcs.gcs_data_writer import GcsDataWriter
             from csvpath.util.gcs.gcs_xlsx_data_reader import GcsXlsxDataReader
-        except Exception:
-            ...
+        except ImportError:
+            print(traceback.format_exc())
         #
         # results readers
         #
@@ -57,8 +54,8 @@ class Hidden:
             from csvpath.managers.results.readers.file_printouts_reader import FilePrintoutsReader
             from csvpath.managers.results.readers.file_lines_reader import FileLinesReader
             from csvpath.managers.results.readers.file_unmatched_reader import FileUnmatchedReader
-        except Exception:
-            ...
+        except ImportError:
+            print(traceback.format_exc())
         #
         # integrations
         #
@@ -86,7 +83,7 @@ class Hidden:
             from csvpath.managers.integrations.ol.result_listener_ol import OpenLineageResultListener
             from csvpath.managers.integrations.ol.results_listener_ol import OpenLineageResultsListener
             from csvpath.managers.integrations.slack.sender import SlackSender
-        except Exception:
-            ...
+        except ImportError:
+            print(traceback.format_exc())
 
 
