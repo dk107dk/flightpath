23a446615944a557c7c933ea41f5229db36d81c6
David Kershaw <dk107dk@hotmail.com>
2026-03-27 16:47:51 -0400

    last with loose litellm version

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 echo 'Copying FlightPath Server to local'
 cp ../flightpath_server/builds/*.tar.gz ./assets
 echo 'Building FlightPath Data'
-poetry update
+#
+# don't do this. be more discriminating about updates.
+#
+#poetry update
 poetry build
 
 cp dist/*.whl builds
